bf074bd47f25a7eb48d5107fa7055a07a6758b28
Alex Paulson <alex@lesite.ca>
2013-10-06 19:09:42 -0400

    Used a different approach for Ruby example to improve output

--- fizz.rb
+++ fizz.rb
@@ -5,9 +5,13 @@
 #######################################################################
 
 1.upto(20) do |fuzzybee|
-	print "FuzzyBees" if a = (fuzzybee % 15).zero?
-	print "Fizz" if b = (fuzzybee % 3).zero?
-	print "Buzz" if c = (fuzzybee % 5).zero?
-	print fuzzybee unless (a || b || c)
-	print "\n"
+	if (fuzzybee % 15).zero?
+		puts "FuzzyBees!"
+	elsif (fuzzybee % 3).zero?
+		puts "Fizz"
+	elsif (fuzzybee % 5).zero?
+		puts"Buzz"
+	else
+		puts fuzzybee
+	end
 end
